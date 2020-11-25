546b5390316fcf91625904c2ba4eefa1c208788e
SnehaMercyS <74917234+SnehaMercyS@users.noreply.github.com>
2020-11-25 03:32:09 -0800

    Add files via upload

---
+++ day 2 task.py
@@ -0,0 +1,20 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> (a,b,c)=(3,4,5)
+>>> print(a/10)
+0.3
+>>> print(b*50)
+200
+>>> print(c+60)
+65
+>>> 
+>>> str="sneha,betsy,biju,latha,sahayam"
+>>> print(str.replace("biju","G"))
+sneha,betsy,G,latha,sahayam
+>>> 
+>>> (a,b)=(5,6.16)
+>>> print(float(a))
+5.0
+>>> print(int(b))
+6
+>>> 
